2ea5138f08db10985bfdbce7c675a9453b7ac566
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-08 06:37:41 -0700

    Abstract profiler to a dedicated class

--- command.php
+++ command.php
@@ -4,8 +4,6 @@ if ( class_exists( 'WP_CLI' ) ) {
 	require_once dirname( __FILE__ ) . '/inc/class-command.php';
 	require_once dirname( __FILE__ ) . '/inc/class-formatter.php';
 	require_once dirname( __FILE__ ) . '/inc/class-logger.php';
-	WP_CLI::add_command( 'profile stage', array( 'runcommand\Profile\Command', 'stage' ) );
-	WP_CLI::add_command( 'profile hook', array( 'runcommand\Profile\Command', 'hook' ) );
-	WP_CLI::add_command( 'profile eval', array( 'runcommand\Profile\Command', 'eval_' ) );
-	WP_CLI::add_command( 'profile eval-file', array( 'runcommand\Profile\Command', 'eval_file' ) );
+	require_once dirname( __FILE__ ) . '/inc/class-profiler.php';
+	WP_CLI::add_command( 'profile', 'runcommand\Profile\Command' );
 }
