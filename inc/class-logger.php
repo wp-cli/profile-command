99eea12b19ab0d88cadf4c6676c5fd1ee022a083
Wojciech Smoliński <wojsmol@wp.pl>
2018-08-10 19:18:48 +0200

    phpcs

--- inc/class-logger.php
+++ inc/class-logger.php
@@ -4,29 +4,29 @@ namespace runcommand\Profile;
 
 class Logger {
 
-	public $time = 0;
-	public $query_count = 0;
-	public $query_time = 0;
-	public $cache_hits = 0;
-	public $cache_misses = 0;
-	public $cache_ratio = null;
-	public $hook_count = 0;
-	public $hook_time = 0;
+	public $time          = 0;
+	public $query_count   = 0;
+	public $query_time    = 0;
+	public $cache_hits    = 0;
+	public $cache_misses  = 0;
+	public $cache_ratio   = null;
+	public $hook_count    = 0;
+	public $hook_time     = 0;
 	public $request_count = 0;
-	public $request_time = 0;
+	public $request_time  = 0;
 
-	private $start_time = null;
-	private $query_offset = null;
-	private $cache_hit_offset = null;
-	private $cache_miss_offset = null;
-	private $hook_start_time = null;
-	private $hook_depth = 0;
+	private $start_time         = null;
+	private $query_offset       = null;
+	private $cache_hit_offset   = null;
+	private $cache_miss_offset  = null;
+	private $hook_start_time    = null;
+	private $hook_depth         = 0;
 	private $request_start_time = null;
 
 	public static $active_loggers = array();
 
 	public function __construct( $definition = array() ) {
-		foreach( $definition as $k => $v ) {
+		foreach ( $definition as $k => $v ) {
 			$this->$k = $v;
 		}
 	}
@@ -36,12 +36,12 @@ class Logger {
 	 */
 	public function start() {
 		global $wpdb, $wp_object_cache;
-		$this->start_time = microtime( true );
+		$this->start_time   = microtime( true );
 		$this->query_offset = ! empty( $wpdb->queries ) ? count( $wpdb->queries ) : 0;
 		if ( false === ( $key = array_search( $this, self::$active_loggers ) ) ) {
 			self::$active_loggers[] = $this;
 		}
-		$this->cache_hit_offset = ! empty( $wp_object_cache->cache_hits ) ? $wp_object_cache->cache_hits : 0;
+		$this->cache_hit_offset  = ! empty( $wp_object_cache->cache_hits ) ? $wp_object_cache->cache_hits : 0;
 		$this->cache_miss_offset = ! empty( $wp_object_cache->cache_misses ) ? $wp_object_cache->cache_misses : 0;
 	}
 
@@ -69,20 +69,20 @@ class Logger {
 		}
 
 		if ( ! is_null( $this->cache_hit_offset ) && ! is_null( $this->cache_miss_offset ) && isset( $wp_object_cache ) ) {
-			$cache_hits = ! empty( $wp_object_cache->cache_hits ) ? $wp_object_cache->cache_hits : 0;
-			$cache_misses = ! empty( $wp_object_cache->cache_misses ) ? $wp_object_cache->cache_misses : 0;
-			$this->cache_hits = $cache_hits - $this->cache_hit_offset;
+			$cache_hits         = ! empty( $wp_object_cache->cache_hits ) ? $wp_object_cache->cache_hits : 0;
+			$cache_misses       = ! empty( $wp_object_cache->cache_misses ) ? $wp_object_cache->cache_misses : 0;
+			$this->cache_hits   = $cache_hits - $this->cache_hit_offset;
 			$this->cache_misses = $cache_misses - $this->cache_miss_offset;
-			$cache_total = $this->cache_hits + $this->cache_misses;
+			$cache_total        = $this->cache_hits + $this->cache_misses;
 			if ( $cache_total ) {
-				$ratio = ( $this->cache_hits / $cache_total ) * 100;
+				$ratio             = ( $this->cache_hits / $cache_total ) * 100;
 				$this->cache_ratio = round( $ratio, 2 ) . '%';
 			}
 		}
 
-		$this->start_time = null;
-		$this->query_offset = null;
-		$this->cache_hit_offset = null;
+		$this->start_time        = null;
+		$this->query_offset      = null;
+		$this->cache_hit_offset  = null;
 		$this->cache_miss_offset = null;
 		if ( false !== ( $key = array_search( $this, self::$active_loggers ) ) ) {
 			unset( self::$active_loggers[ $key ] );
