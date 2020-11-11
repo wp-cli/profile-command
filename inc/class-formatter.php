4bc71f5c071da45f0195e902baf32ea1e5aa1822
Timothy Jacobs <timothy@ironbounddesigns.com>
2020-11-11 15:26:18 -0500

    Remove default value from $include_total
    This parameter isn't actually optional because it is followed by required parameters. This issues a warning on PHP 8.

--- inc/class-formatter.php
+++ inc/class-formatter.php
@@ -42,7 +42,7 @@ class Formatter {
 	 *
 	 * @param array $items
 	 */
-	public function display_items( $items, $include_total = true, $order, $orderby ) {
+	public function display_items( $items, $include_total, $order, $orderby ) {
 		if ( 'table' === $this->args['format'] && empty( $this->args['field'] ) ) {
 			$this->show_table( $order, $orderby, $items, $this->args['fields'], $include_total );
 		} else {
