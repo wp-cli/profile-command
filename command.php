ab691d266422894936a5f496264f05d9cc1b34f3
Alain Schlesser <alain.schlesser@gmail.com>
2021-04-19 12:42:05 +0100

    Add autoloader

--- command.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( class_exists( 'WP_CLI' ) ) {
-	require_once dirname( __FILE__ ) . '/inc/class-command.php';
-	require_once dirname( __FILE__ ) . '/inc/class-formatter.php';
-	require_once dirname( __FILE__ ) . '/inc/class-logger.php';
-	require_once dirname( __FILE__ ) . '/inc/class-profiler.php';
-	WP_CLI::add_command( 'profile', 'runcommand\Profile\Command' );
-}
